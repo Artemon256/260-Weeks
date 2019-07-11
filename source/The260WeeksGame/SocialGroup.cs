66dfac541c6fe05641d9abedea8fff8ef4e64504
Artemii Bigdan <artem@bigdan.in>
2019-07-11 23:27:41 +0300

    Added SocialGroup.cs

---
+++ source/The260WeeksGame/SocialGroup.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace The260WeeksGame
+{
+    public class SocialGroup : GameMember
+    {
+        public override void Turn() {
+            
+        }
+
+        private static List<SocialGroup> socialGroups = null;
+        public static List<SocialGroup> getSocialGroups() {
+            if (socialGroups == null) {
+                socialGroups = new List<SocialGroup>();
+                // INIT
+            }
+            return socialGroups;
+        }
+    }
+}
